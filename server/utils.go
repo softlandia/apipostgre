e0fc0af3c1d896dfac844eb77ec78b2891473e71
Vladimir <ltvtynmtd93@yandex.ru>
2023-10-08 12:39:56 +0300

    нет подклчения к бд

--- server/utils.go
+++ server/utils.go
@@ -3,7 +3,7 @@ package server
 import "time"
 
 func DoWithTries(fn func() error, attemtps int, delay time.Duration) (err error) {
-	for attemtps < 0 {
+	for attemtps > 0 {
 		if err = fn(); err != nil {
 			time.Sleep(delay)
 			attemtps--
