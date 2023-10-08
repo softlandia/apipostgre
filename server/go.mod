fefda39f8094c3aa68def36c1bfa7a2608617c0f
Vladimir <ltvtynmtd93@yandex.ru>
2023-10-08 13:02:19 +0300

    в папке сервер main

---
+++ server/go.mod
@@ -0,0 +1,19 @@
+module main
+
+go 1.21.0
+
+require github.com/julienschmidt/httprouter v1.3.0
+
+require (
+	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
+	github.com/jackc/pgconn v1.14.1 // indirect
+	github.com/jackc/pgio v1.0.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgproto3/v2 v2.3.2 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgx/v5 v5.4.3 // indirect
+	github.com/jackc/puddle/v2 v2.2.1 // indirect
+	golang.org/x/crypto v0.9.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
+)
---
+++ server/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"log"
+	"main/app/control"
+	"main/app/model"
+	"net/http"
+
+	"main/app/server"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func main() {
+
+	r := httprouter.New()
+	routes(r)
+
+	err := http.ListenAndServe(":8080", r)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	mdl := model.GetStorage()
+
+	postgreSqlClient, err := server.NewClient(context.TODO(), 3, mdl.Storage)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func routes(r *httprouter.Router) {
+	//путь к папке со внешними файлами: html, js, css, изображения и т.д.
+	r.ServeFiles("/app/public/*filepath", http.Dir("public"))
+	//что следует выполнять при входящих запросах указанного типа и по указанному адресу
+	r.GET("/", control.StartPage)
+	r.GET("/users", control.GetUsers)
+}
